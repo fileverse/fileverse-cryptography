4d262ee576f737b0f8fd47a6931fb7c65bf49e9b
Nadeem <nadeem@fileverse.io>
2025-05-28 02:07:21 +0530

    fix default encoding

--- src/utils/encoding.ts
+++ src/utils/encoding.ts
@@ -2,7 +2,7 @@ import { fromUint8Array, toUint8Array } from "js-base64";
 import { EncodingType, EncodedReturnType } from "../types";
 
 export const toBytes = (base64: string) => toUint8Array(base64);
-export const bytesToBase64 = (bytes: Uint8Array, urlSafe = true) =>
+export const bytesToBase64 = (bytes: Uint8Array, urlSafe = false) =>
   fromUint8Array(bytes, urlSafe);
 
 export function encodeData<E extends EncodingType>(
